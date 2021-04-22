594a2dbca7b0d26aad7ec455c59a0ccb68c265eb
Kunaal Jha <jha@cs.utexas.edu>
2021-04-22 17:45:12 -0500

    Progress to vector register file. Not yet funcitonal

--- vregs.v
+++ vregs.v
@@ -1,29 +1,33 @@
 `timescale 1ps/1ps
 
-/*  Specifications
-    - 16 64-element, 16-bit vector registers
+/*  Specifications of Vector Register File
+    - 16 16-element, 16-bit vector registers
+    - Reads have 0 cycle latency
+    - Writes have 1 cycle latency
+    - 2 read ports return an entire vector register with its length
+    - A third read port retuns one entry of a vector register
+    - 1 write port allows writing one entry of a vector register
 */
 
 module vregs(input clk,
-    input [5:0]raddr0_, output [1023:0]rdata0,
-    input [5:0]raddr1_, output [1023:0]rdata1,
-    input wen, input [5:0]waddr, input [1023:0]wdata);
+    input [3:0]rAddr0, output [255:0]rData0, output rLen0,
+    input [3:0]rAddr1, output [255:0]rData1, output rLen1,
+    input [3:0]rAddr2, input [3:0]rInd2, output [15:0]rData2, 
+    input wEn, input [3:0]wAddr, input [3:0]wInd, input[15:0]wData);
 
 
-    reg [63:0]data[0:63][0:15];
+    reg [255:0]data[0:15];
 
-    reg [3:0]raddr0;
-    reg [3:0]raddr1;
+    assign rData0 = data[rAddr0];
+    assign rData1 = data[rAddr1];
+    wire rBitNum = rInd2 * 16;
+    assign rdata2 = (data[raddr2])[rbitNum+15:rbitNum];
 
-    assign rdata0 = data[raddr0][0];
-    assign rdata1 = data[raddr1][0];
-
+    wire wBitNum = wind * 16;
     always @(posedge clk) begin
         //$write("%d\n", data[0]);
-        raddr0 <= raddr0_;
-        raddr1 <= raddr1_;
         if (wen) begin
-            data[waddr][0] <= wdata;
+            (data[waddr])[wBitNum+15:wBitNum] <= wData;
         end
     end
 
